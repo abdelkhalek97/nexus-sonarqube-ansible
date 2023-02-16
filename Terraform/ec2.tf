bfd50c156b02b021d7053dad54c75a115e419393
abdelkhalek97 <ahmed100119@hotmail.com>
2023-02-17 00:28:30 +0200

    edit2

--- Terraform/ec2.tf
+++ Terraform/ec2.tf
@@ -28,7 +28,7 @@ resource "aws_instance" "private-ec2-1" {
 }
 
 resource "aws_instance" "private-ec2-2" {
- ami           = "ami-0557a15b87f6559cf"
+ ami           = "ami-0dfcb1ef8550277af"
   instance_type = "t3.micro"
   associate_public_ip_address = false
   subnet_id = aws_subnet.private-sub2.id
